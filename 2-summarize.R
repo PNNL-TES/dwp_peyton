bdf95d54d0f57f299409b6fa75fe941705f16df9
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2015-07-28 09:39:40 -0400

    Smart min/max finding - max AFTER min

--- 2-summarize.R
+++ 2-summarize.R
@@ -52,8 +52,7 @@ rawdata$samplenum <- cumsum(!oldsampleflag)
 printlog("Computing elapsed seconds...")
 rawdata_samples <- rawdata %>%
   group_by(samplenum) %>%
-  mutate(elapsed_seconds = (FRAC_HRS_SINCE_JAN1 - min(FRAC_HRS_SINCE_JAN1)) * 60 * 60) #%>%
-#  filter(elapsed_seconds >= min(FLUXWINDOW_S) & elapsed_seconds <= max(FLUXWINDOW_S))
+  mutate(elapsed_seconds = (FRAC_HRS_SINCE_JAN1 - min(FRAC_HRS_SINCE_JAN1)) * 60 * 60) 
 
 # Load MPVPosition map
 printlog("Loading valve map data...")
@@ -101,7 +100,7 @@ matchfun <- function(DATETIME, MPVPosition) {
 printlog( "Computing summary statistics for each sample..." )
 # The window in which we look for min and max concentrations
 MAX_MINCONC_TIME <- 10  # the minimum concentration has to occur in first 10 s
-MIN_MAXCONC_TIME <- 2  # the maximum concentration can't occur in first 2 s
+MAX_MAXCONC_TIME <- 45  # the maximum concentration can't occur in first 2 s
 
 # We want to apply different criteria here, so three different pipelines
 # to compute the min and max gas concentrations
@@ -115,16 +114,32 @@ summarydata_min <- rawdata_samples %>%
     min_CH4_time = nth(elapsed_seconds, which.min(CH4_dry))
   )
 
-summarydata_max <- rawdata_samples %>%
-  filter(elapsed_seconds >= MIN_MAXCONC_TIME) %>%
-  group_by(samplenum) %>%
+# Now we want to look for the max concentration AFTER the minimum
+rawdata_temp <- rawdata_samples %>%
+  left_join(summarydata_min) 
+summarydata_maxCO2 <- rawdata_temp %>%
+  filter(elapsed_seconds > min_CO2_time & elapsed_seconds < MAX_MAXCONC_TIME) %>%
   summarise(
     max_CO2 = max(CO2_dry),
-    max_CO2_time = nth(elapsed_seconds, which.max(CO2_dry)),
+    max_CO2_time = nth(elapsed_seconds, which.max(CO2_dry))
+  )
+summarydata_maxCH4 <- rawdata_temp %>%
+  filter(elapsed_seconds > min_CH4_time & elapsed_seconds < MAX_MAXCONC_TIME) %>%
+  summarise(
     max_CH4 = max(CH4_dry),
     max_CH4_time = nth(elapsed_seconds, which.max(CH4_dry))
   )
 
+# summarydata_max <- rawdata_samples %>%
+#   filter(elapsed_seconds >= MIN_MAXCONC_TIME) %>%
+#   group_by(samplenum) %>%
+#   summarise(
+#     max_CO2 = max(CO2_dry),
+#     max_CO2_time = nth(elapsed_seconds, which.max(CO2_dry)),
+#     max_CH4 = max(CH4_dry),
+#     max_CH4_time = nth(elapsed_seconds, which.max(CH4_dry))
+#   )
+# 
 summarydata_other <- rawdata_samples %>%
   group_by(samplenum) %>%
   summarise(
@@ -137,8 +152,12 @@ summarydata_other <- rawdata_samples %>%
 
 # Merge pieces together to form final summary data set
 summarydata <- summarydata_other %>%
-  left_join(summarydata_max, by="samplenum") %>% 
-  left_join(summarydata_min, by="samplenum")
+  left_join(summarydata_min, by="samplenum") %>%
+  left_join(summarydata_maxCO2, by="samplenum") %>% 
+  left_join(summarydata_maxCH4, by="samplenum")
+# summarydata <- summarydata_other %>%
+#   left_join(summarydata_max, by="samplenum") %>% 
+#   left_join(summarydata_min, by="samplenum")
 
 printlog("Merging Picarro and mapping data...")
 summarydata <- left_join(summarydata, valvemap, by=c("MPVPosition", "valvemaprow"), all.x=TRUE)
